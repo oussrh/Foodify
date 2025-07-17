308e8d7e523325566facef96ba6f27501d8fd099
Rhoni oussama <ousrh7@gmail.com>
2025-07-17 15:03:13 +0200

    Update page.ts

--- types/page.ts
+++ types/page.ts
@@ -1,9 +1,8 @@
 // types/page.ts
-
 export interface TypedPageProps<
   Params extends Record<string, string> = {},
   SearchParams extends Record<string, string | string[] | undefined> = {}
 > {
-  params: Params
-  searchParams?: SearchParams
+  params: Promise<Params>
+  searchParams?: Promise<SearchParams>
 }
